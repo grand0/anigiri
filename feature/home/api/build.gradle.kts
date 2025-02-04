b5fad30bfa6930f64489e7f25b024c3bf2db9479
iponom <iponom0420@gmail.com>
2025-02-04 20:59:19 +0300

    removed redundant dependency

--- feature/home/api/build.gradle.kts
+++ feature/home/api/build.gradle.kts
@@ -38,7 +38,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":core:nav"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
